37f0a743c9e70b0930d7a9d936d0adadbee4f01f
Bora Kim <borakim@boras-mbp.mynet>
2020-11-20 16:03:39 +0000

    save

---
+++ app.js
@@ -0,0 +1,38 @@
+let dice0, dice1, score, evenOdd;
+// maximum number for dice is 6
+let diceDOM0 = document.querySelector('.dice0');
+let diceDOM1 = document.querySelector('.dice1');
+// User select Even/Odd from drop down box
+// User click 'Roll dice' button
+// Roll dice function => when roll button clicked, two dices will roll (dice0, dice1)
+// Check the winner 
+// Give the score to the winner
+
+document.querySelector(".btn-roll").addEventListener('click', function rollDice(){
+    dice0 = Math.floor(Math.random() * 6) + 1;
+    dice1 = Math.floor(Math.random() * 6) + 1;
+    console.log(dice0,dice1)
+
+    diceDOM0.src = 'dice'+ dice0+'.png';    
+    diceDOM1.src = 'dice'+ dice1+'.png';
+
+    score = dice0 + dice1;
+    if(score % 2 === 0) {evenOdd = 2;} else {evenOdd =1;}
+    checkWinner();
+});
+
+function checkWinner () {
+    // check User input Even or Odd?
+    document.querySelector(".even").addEventListener('onclick', function winner () {
+        if(evenOdd === 2) {
+            console.log('you are right')
+        }
+    });
+    // check dice total
+    // compare User input and dice total 
+    // If User guessed dice total number correctly => User wins the game 
+};
+
+document.querySelector(".btn-new").addEventListener('click', function reSet(){
+    //If user clicks 'new game' then reset score 
+});
