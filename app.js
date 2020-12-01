aac984fbc575aeb48e6f6de0718107affe064b41
Serin <serin0837@yahoo.com>
2020-12-01 16:39:26 +0000

    add description and reload page function

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ document.querySelector(".even").addEventListener('click', ()=>{evenOdd = 2});
 document.querySelector(".odd").addEventListener('click', ()=>{evenOdd = 1});
 
 document.querySelector(".btn-roll").addEventListener('click', function rollDice(){
+    if(evenOdd === undefined){
+        alert("you need to decide even or odd")
+    }else{
     dice0 = Math.floor(Math.random() * 6) + 1;
     dice1 = Math.floor(Math.random() * 6) + 1;
     console.log(dice0,dice1)
@@ -15,32 +18,34 @@ document.querySelector(".btn-roll").addEventListener('click', function rollDice(
 
     score = dice0 + dice1;
     if(score % 2 === 0 && evenOdd == 2) {
-        console.log('You are right ')
+        alert('You are right +100円')
         userMoney[1] += 100;
         let x = document.getElementById("score-1");
         x.innerHTML = userMoney[1];
     } else if(score % 2 === 1 && evenOdd == 1) {
-        console.log('You are right ')
+        alert('You are right +100円')
         userMoney[1] += 100;
         let x = document.getElementById("score-1");
         x.innerHTML = userMoney[1];
     }else { 
-        console.log('You are wrong ');
+        alert('You are wrong +100円 for player1');
         userMoney[0] += 100;
         let y = document.getElementById("score-0");
         y.innerHTML = userMoney[0];
     }
+    
     endGame();
+    }
+    
 });
 
 function endGame (){
     if(userMoney[0] == 1000 || userMoney[1] == 1000) {
-        console.log('Game end')
+        alert('Game over')
+        window.location.reload(true)
     }
     // const button = document.querySelector('button')
     // button.disabled = true
 }
 
-document.querySelector(".btn-new").addEventListener('click', function reSet(){
-    //If user clicks 'new game' then reset score 
-});
+
